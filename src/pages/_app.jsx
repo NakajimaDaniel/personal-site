2c32977ffe79994184ca71264f89c3e185cab81f
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-09-13 19:33:58 -0300

    fix: removed title from _document & added to _app

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,7 +2,14 @@ import "../styles/global.scss"
 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} /> 
+  return (
+    <>
+      <Head>
+        <title>Personal Site</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -8,7 +8,6 @@ export default class myDocument extends Document{
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
-          <title>Daniel Nakajima</title> 
         </Head>
         <body>
           <Main/>
