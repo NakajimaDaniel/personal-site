1bb97299144317162d260bfbca8b456969cf8602
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-09-20 19:49:00 -0300

    feat: projects page

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -12,24 +12,55 @@ export default function Projects() {
       <div className={styles.MainContainer}>
         <h1>Personal Projects</h1>
 
-        <div className={styles.ProjectBox} >
-          <span>NX-Weather</span>
-          <div className={styles.ImgWrapper} >
-            <img src="nxweather1.png" alt="" />
-   
-            <div className={styles.ExtLink}>
-              <img src="launch.svg" alt="" />
-              <p>project.com</p>
+        <div className={styles.ProjectsContainer}>
+          <div className={styles.ProjectBox} >
+            <span>NX-Weather</span>
+            <div className={styles.ImgWrapper} >
+              <img src="nxweather1.png" alt="" />
+    
+              <div className={styles.ExtLink}>
+                <img src="launch.svg" alt="" />
+                <p>nx-weather.vercel.app</p>
+              </div>
+              
             </div>
-            
-            {/* <div className={styles.ExtLink}>
-              <span><img src="launch.svg" alt="" />Teste</span>
-            </div> */}
-
+            <p>Simple weather (current and forecast) app.</p>
+            <p>HTML | CSS | Javascript | React</p>
           </div>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <p>HTML | CSS | Javascript | React</p>
+
+          <div className={styles.ProjectBox} >
+            <span>Simple Form</span>
+            <div className={styles.ImgWrapper} >
+              <img src="simpleform.png" alt="" />
+    
+              <div className={styles.ExtLink}>
+                <img src="launch.svg" alt="" />
+                <p>simple-form-mu.vercel.app</p>
+              </div>
+              
+            </div>
+            <p> Simple form created with HTML, CSS, JS only. Its possible to register user in database (IndexedDB), show a list of registered users and delete user in database. </p>
+            <p> HTML | CSS | Javascript </p>
+          </div>
+          
+          <div className={styles.ProjectBox} >
+            <span>LetmeAsk</span>
+            <div className={styles.ImgWrapper} >
+              <img src="letmeask.png" alt="" />
+    
+              <div className={styles.ExtLink}>
+                <img src="launch.svg" alt="" />
+                <p>letmeask-kappa-swart.vercel.app</p>
+              </div>
+              
+            </div>
+            <p>Simple app for ask a question</p>
+            <p> HTML | CSS | Javascript | ReactJs | Firebase | TailwindCSS </p>
+          </div>
+
         </div>
+
+
 
 
       </div>
