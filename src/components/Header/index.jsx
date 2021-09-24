82ddf2fe45bdcaced31e8b836abe3c94779aff1e
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-09-24 19:33:03 -0300

    fix: scroll effect

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,8 +2,8 @@
 import styles from './style.module.scss'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { ActiveLink } from '../ActiveLink'
 
+import {Link as LinkScroll} from 'react-scroll'
 
 export function Header() {
 
@@ -15,15 +15,15 @@ export function Header() {
         <img src="logo.svg" alt="logo image" />
       </Link>
       <nav className={styles.HeaderMenu}>
-        <ActiveLink href="/" activeClassName={styles.active}>
+        <LinkScroll to="home" spy={true}  activeClassName={styles.active}>
           <span>Home</span>
-        </ActiveLink>
-        <ActiveLink href="/projects" activeClassName={styles.active} prefetch>
+        </LinkScroll>
+        <LinkScroll to="projects" spy={true}  activeClassName={styles.active} >
           <span>Projects</span>
-        </ActiveLink>
-        <ActiveLink href="/contact" activeClassName={styles.active} prefetch>
+        </LinkScroll>
+        <LinkScroll to="contact" spy={true}  activeClassName={styles.active} >
           <span>Contact</span>
-        </ActiveLink>
+        </LinkScroll>
       </nav>
     </div>
   )
