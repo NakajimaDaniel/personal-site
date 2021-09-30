4c65012d3dd7a56cbaebe04dd31e366c0f6d7e7d
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-09-30 19:09:20 -0300

    added meta viewport

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Personal Site</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </Head>
       <Component {...pageProps} />
     </>
