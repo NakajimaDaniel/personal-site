b971a4a4f0309e3e31fe35a88206c6062ca44710
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-10-01 19:04:38 -0300

    fix: adj responsiveness

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,35 +1,70 @@
 import styles from './style.module.scss';
 import Link from 'next/link';
+import { useDimensions } from '../../hooks/useDimensions';
 
 
 export default function Home() {
+
+  const { width, heigth } = useDimensions();
+
+
+
   return (
     <div id="home">
-      <div className={styles.HomeContainer}>
-        <img src="homeImg.svg" alt="" />
+      {width <= 1100 ? (
+        <div className={styles.HomeContainerMobile} >
+          
+          <div className={styles.HomeTextArea}>
+                <h3>Hi, my name is</h3>
+                <h4>Daniel</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  Quisque sit amet imperdiet odio, sodales ultricies ipsum. 
+                  In vel libero vitae nisl maximus convallis nec vel magna. 
+                </p>
+      
+                <div className={styles.socialLink}>
+                  <Link href="/" >
+                    <img src="github_icon.svg" alt="github social link" />
+                  </Link>
+                  <Link href="/" >
+                    <img src="linkedin_social_icon.svg" alt="linkedin social link" />
+                  </Link>
+                </div>
+          </div>
 
-        <div className={styles.HomeTextArea}>
-          <h3>Hi, my name is</h3>
-          <h4>Daniel</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Quisque sit amet imperdiet odio, sodales ultricies ipsum. 
-            In vel libero vitae nisl maximus convallis nec vel magna. 
-          </p>
-
-          <div className={styles.socialLink}>
-            <Link href="/" >
-              <img src="github_icon.svg" alt="github social link" />
-            </Link>
-            <Link href="/" >
-              <img src="linkedin_social_icon.svg" alt="linkedin social link" />
-            </Link>
+          <div  className={styles.mobileImg} >
+            <img src="homeImgMobile.svg" alt="" />
+            <img src="homeImgRightMobile.svg" alt="" />
           </div>
 
         </div>
-        <img src="homeImgRight.svg" alt="" /> 
-
-
-      </div>
+      ) : (
+              <div className={styles.HomeContainer}>
+              <img src="homeImg.svg" alt="" />
+      
+              <div className={styles.HomeTextArea}>
+                <h3>Hi, my name is</h3>
+                <h4>Daniel</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  Quisque sit amet imperdiet odio, sodales ultricies ipsum. 
+                  In vel libero vitae nisl maximus convallis nec vel magna. 
+                </p>
+      
+                <div className={styles.socialLink}>
+                  <Link href="/" >
+                    <img src="github_icon.svg" alt="github social link" />
+                  </Link>
+                  <Link href="/" >
+                    <img src="linkedin_social_icon.svg" alt="linkedin social link" />
+                  </Link>
+                </div>
+      
+              </div>
+              <img src="homeImgRight.svg" alt="" /> 
+      
+      
+            </div>
+      )}
     </div>
   )
 }
