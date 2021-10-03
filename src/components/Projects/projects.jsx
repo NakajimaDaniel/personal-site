a9ff83e3a10c227664f8049723f31a3d1ec1f1a5
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-10-03 16:28:54 -0300

    fix: added link to img

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -2,9 +2,14 @@ import { Header } from "../Header";
 import styles from './projects.module.scss';
 
 import Link from 'next/link';
+import { useDimensions } from "../../hooks/useDimensions";
 
 
 export default function Projects() {
+
+  const { width, height } = useDimensions();
+
+
   return (
     <div id="projects">
    
@@ -12,21 +17,23 @@ export default function Projects() {
       <div className={styles.MainContainer}>
         <span> # Personal Projects</span>
         <div className={styles.ProjectsMainWrapper}>
-
-          <img src="projectLeftImg.svg" alt="" />
-
+          {width >= 950 ? (<img src="projectLeftImg.svg" alt="" />) : (<div></div>)}
+          
           <div className={styles.ProjectsContainer}>
             <div className={styles.ProjectBox} >
               <span>NX-Weather</span>
-              <div className={styles.ImgWrapper} >
-                <img src="nxweather1.png" alt="" />
-      
-                <div className={styles.ExtLink}>
-                  <img src="launch.svg" alt="" />
-                  <p>nx-weather.vercel.app</p>
+              <Link href="https://nx-weather.vercel.app">
+                <div className={styles.ImgWrapper} >
+                  <img src="nxweather1.png" alt="" />
+                  {width >= 950 ? (
+                    <div className={styles.ExtLink}>
+                      <img src="launch.svg" alt="" />
+                      <p>nx-weather.vercel.app</p>
+                    </div>
+                  ) : (<div></div>)}
                 </div>
-                
-              </div>
+              </Link>
+
               <div className={styles.ProjectBoxText} >
                 <p>Simple weather (current and forecast) app.</p>
                 <p>HTML  CSS  Javascript  React</p>
@@ -36,15 +43,18 @@ export default function Projects() {
 
             <div className={styles.ProjectBox} >
               <span>Simple Form</span>
-              <div className={styles.ImgWrapper} >
-                <img src="simpleform.png" alt="" />
-      
-                <div className={styles.ExtLink}>
-                  <img src="launch.svg" alt="" />
-                  <p>simple-form-mu.vercel.app</p>
+              <Link href="https://simple-form-mu.vercel.app" >
+                <div className={styles.ImgWrapper} >
+                  <img src="simpleform.png" alt="" />
+                  {width >= 950 ? (
+                    <div className={styles.ExtLink}>
+                      <img src="launch.svg" alt="" />
+                      <p>simple-form-mu.vercel.app</p>
+                    </div>
+                  ) : (<div></div>)}
                 </div>
-                
-              </div>
+              </Link>
+
               <div className={styles.ProjectBoxText}>
                 <p> Simple form created with HTML, CSS, JS only. Its possible to register user in database (IndexedDB), show a list of registered users and delete user in database. </p>
                 <p> HTML  CSS  Javascript </p>
@@ -53,27 +63,29 @@ export default function Projects() {
             </div>
             
           </div>
-
-          <img src="projectRightImg.svg" alt="" />
+          {width >= 950 ? (<img src="projectRightImg.svg" alt="" />) : (<div></div>)}
+          
         </div>
 
 
         <div className={styles.ProjectsMainWrapper}>
 
-          <img src="projectLeftImg.svg" alt="" />
+        {width >= 950 ? (<img src="projectLeftImg.svg" alt="" />) : (<div></div>)}
 
           <div className={styles.ProjectsContainer}>
             <div className={styles.ProjectBox} >
               <span>LetmeAsk</span>
-              <div className={styles.ImgWrapper} >
-                <img src="letmeask.png" alt="" />
-      
-                <div className={styles.ExtLink}>
-                  <img src="launch.svg" alt="" />
-                  <p>letmeask-kappa-swart.vercel.app</p>
+              <Link href="https://letmeask-kappa-swart.vercel.app">
+                <div className={styles.ImgWrapper} >
+                  <img src="letmeask.png" alt="" />
+                  {width >= 950 ? (
+                    <div className={styles.ExtLink}>
+                      <img src="launch.svg" alt="" />
+                      <p>letmeask-kappa-swart.vercel.app</p>
+                    </div>
+                  ) : (<div></div>)}
                 </div>
-                
-              </div>
+              </Link>
               <div className={styles.ProjectBoxText}>
                 <p>Simple app for ask a question</p>
                 <p> HTML  CSS  Javascript  ReactJs  Firebase  TailwindCSS </p>
@@ -84,12 +96,13 @@ export default function Projects() {
               <span>Simple Form</span>
               <div className={styles.ImgWrapper} >
                 <img src="simpleform.png" alt="" />
+                {width >= 950 ? (
+                  <div className={styles.ExtLink}>
+                    <img src="launch.svg" alt="" />
+                    <p>simple-form-mu.vercel.app</p>
+                  </div>
+                ) : (<div></div>)}
 
-                <div className={styles.ExtLink}>
-                  <img src="launch.svg" alt="" />
-                  <p>simple-form-mu.vercel.app</p>
-                </div>
-                
               </div>
               <div className={styles.ProjectBoxText}>
                 <p> Simple form created with HTML, CSS, JS only. Its possible to register user in database (IndexedDB), show a list of registered users and delete user in database. </p>
@@ -100,7 +113,7 @@ export default function Projects() {
             
           </div>
 
-          <img src="projectRightImg.svg" alt="" />
+          {width >= 950 ? (<img src="projectRightImg.svg" alt="" />) : (<div></div>)}
           </div>
 
 
