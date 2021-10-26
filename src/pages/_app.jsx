2f2a3d61cbf326c19aa78017d311d76ead86afec
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-10-26 18:40:51 -0300

    added chakraui and provider around app

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,18 @@
 import "../styles/global.scss"
 import Head from 'next/head'
+import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+    <ChakraProvider>
       <Head>
-        <title>Daniel Nakajima | Personal Site</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+          <title>Daniel Nakajima | Personal Site</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </Head>
       <Component {...pageProps} />
+    </ChakraProvider>
+
     </>
   )
 }
