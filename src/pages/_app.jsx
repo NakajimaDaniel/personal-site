08207cf8b3aeaa728e214ad6b728610ca27474af
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-10-26 19:30:07 -0300

    feat: added custom theme

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,12 @@
 import "../styles/global.scss"
 import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react';
+import MainTheme from '../themes/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <ChakraProvider>
+    <ChakraProvider theme={MainTheme}>
       <Head>
           <title>Daniel Nakajima | Personal Site</title>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
---
+++ src/themes/theme.ts
@@ -0,0 +1,19 @@
+import { extendTheme } from "@chakra-ui/react"
+
+
+const colors = {
+  black: {
+    "900": "#1F1F1F",
+  },
+  yellow: {
+    "500": "#FFC700",
+  },
+  gray: {
+    "700": "#505050",
+    "500": "#A8A8A8",
+    "400": "#D1D1D1",
+  }
+}
+
+
+export const MainTheme = extendTheme({ colors })
