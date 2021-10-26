1f6323f8150ef74c43d5cec465961bc635706d78
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-10-26 18:29:16 -0300

    fix: changed site title

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Personal Site</title>
+        <title>Daniel Nakajima | Personal Site</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </Head>
       <Component {...pageProps} />
