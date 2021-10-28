32ab404be57a2704e2613d6d7a328e66b09fd650
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-10-28 19:50:30 -0300

    fix: fixed theme import issue

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,7 @@
 
 import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react';
-import theme from '../theme'
+import {theme} from '../theme'
 
 function MyApp({ Component, pageProps }) {
   return (
