127c517cc09b9f80263e7f0fe12c093f37ab664d
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-10-30 18:14:22 -0300

    fix: removed comments and added project link

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,7 @@
 
-// // import styles from './style.module.scss'
-// import Link from 'next/link'
-// import { useRouter } from 'next/router'
+import Link from 'next/link'
 
-// import {Link as LinkScroll} from 'react-scroll'
 import { useState } from 'react';
-// import Modal from 'react-modal';
-
 
 
 import { useDimensions } from '../../hooks/useDimensions';
@@ -15,78 +10,9 @@ import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
 
 import { Container, Box, useColorModeValue, Flex, Spacer, Image, Button, useColorMode, Menu, MenuButton, IconButton, MenuList, MenuItem } from '@chakra-ui/react';
 
+
+
 export function Header() {
-
-
-  const [activeHomeLink, setActiveHomeLink] = useState(false);
-  const [activeProjectLink, setActiveProjectLink] = useState(false);
-  const [activeContactLink, setActiveContactLink] = useState(false);
-  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
-
-
-  // const { width, heigth } = useDimensions();
-
-
-
-  // return (
-  //   <div className={styles.MainContainer}>
-  //     <Link href="/" >
-  //       <img src="logo.svg" alt="logo image" />
-  //     </Link>
-  //     { width >= 1000 ? (
-  //       <nav className={styles.HeaderMenu}>
-  //       <LinkScroll to="home" spy={true}  smooth="easeOutQuint"   onSetActive={() => setActiveHomeLink(true)}  onSetInactive={() => setActiveHomeLink(false)} >
-  //         <span className={activeHomeLink ? styles.active : ''} >Home</span>
-  //       </LinkScroll>
-  //       <LinkScroll to="projects" spy={true}  smooth="easeOutQuint"   onSetActive={() => setActiveProjectLink(true)}   onSetInactive={() => setActiveProjectLink(false)} >
-  //         <span className={activeProjectLink ? styles.active : ''} >Projects</span>
-  //       </LinkScroll>
-  //       <LinkScroll to="contact" spy={true}   smooth="easeOutQuint"    onSetActive={() => setActiveContactLink(true)}  onSetInactive={() => setActiveContactLink(false)} >
-  //         <span className={activeContactLink ? styles.active : ''} >Contact</span>
-  //       </LinkScroll>
-  //     </nav>
-  //     ) : (
-  //       <div className={styles.mobileMenu} >
-  //         {isSideBarOpen? (
-  //           <a onClick={() => setIsSideBarOpen(false)}> 
-  //             <img src="menu.svg" alt="" /> 
-  //           </a>
-  //         ) : ( 
-  //         <a onClick={() => setIsSideBarOpen(true)}> 
-  //           <img src="menu.svg" alt="" /> 
-  //         </a>)}
-   
-
-  //         <div className={isSideBarOpen ? styles.sideBarOpen : styles.sideBar}>
-  //           <div className={styles.sideBarContent} >
-  //             <a onClick={() => setIsSideBarOpen(false)}>
-  //               <img src="close.svg" alt="" />
-  //             </a>
-  //             <nav>
-  //               <div>
-  //                 <LinkScroll to="home" spy={true} smooth="easeOutQuint" >
-  //                   <a > Home </a>
-  //                 </LinkScroll>
-  //                 <LinkScroll to="projects" spy={true} smooth="easeOutQuint" >
-  //                   <a > Projects </a>
-  //                 </LinkScroll>
-  //                 <LinkScroll to="contact" spy={true} smooth="easeOutQuint" >
-  //                   <a > Contact </a>
-  //                 </LinkScroll>
-
-
-  //               </div>
-  //             </nav>
-  //           </div>
-  //         </div>
-
-  //       {isSideBarOpen ? (<div className={styles.mobileBackground} onClick={() => setIsSideBarOpen(false)} ></div>) : (<div></div>)}
-          
-
-  //       </div>
-  //     ) }
-  //   </div>
-  // )
 
   const { colorMode, toggleColorMode } = useColorMode();
   const { width, height } = useDimensions();
@@ -106,16 +32,16 @@ export function Header() {
         { width >= 655 ? (
           <>
             <Box pr="5">
-              Home
+            <Link href="/">Home</Link>
             </Box>
             <Box pr="5">
-              Projects
+            <Link href="projects">Projects</Link>
             </Box>
             <Box pr="5">
               Posts
             </Box>
             <Box pr="5">
-              Contact
+              <Link href="contact">Contact</Link>
             </Box>
 
             <Box pr="10">
