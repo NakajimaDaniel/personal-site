6b6f48b86e991a277ceddf7bf9fe591c35554381
NakajimaDaniel <nakajimadaniel96@gmail.com>
2022-02-15 20:32:35 -0300

    fix: justify text

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ import { Container, Flex, Text, Image, Box } from "@chakra-ui/react";
 export function Footer() {
   return (
     <Container mb={5} mt={5}>
-      <Flex align="center" flexDir="row">
+      <Flex align="center" flexDir="row" justify="center" >
         <Text pr="1">©  Design with</Text>
         <Image src="heart.svg" />
         <Text pl="1">by Daniel Nakajima. All Rights Reserved.</Text>
