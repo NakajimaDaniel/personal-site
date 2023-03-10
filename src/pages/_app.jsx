c45ad7896716e79944ddbd7a5b7cb7f27c5528cb
NakajimaDaniel <nakajimadaniel96@gmail.com>
2023-03-10 12:02:01 -0300

    next and react upgraded and prismic install

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,18 +3,25 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import {theme} from '../theme'
 import { Layout } from '../components/Layout';
-import { motion, AnimatePresence } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion";
+import Link from 'next/link'
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import { repositoryName } from '../prismicio'
 
 function MyApp({ Component, pageProps, router }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <AnimatePresence exitBeforeEnter initial={true}>
-          <Component {...pageProps} key={router.route} />
-        </AnimatePresence>
-      </Layout>
-      
-    </ChakraProvider>
+    <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
+      <PrismicPreview repositoryName={repositoryName}>
+        <ChakraProvider theme={theme}>
+          <Layout>
+            <AnimatePresence exitBeforeEnter initial={true}>
+              <Component {...pageProps} key={router.route} />
+            </AnimatePresence>
+          </Layout> 
+        </ChakraProvider>
+      </PrismicPreview>
+    </PrismicProvider>
   )
 }
 
