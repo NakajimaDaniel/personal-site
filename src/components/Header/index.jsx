39bf377ed60fbb4dc863dcd83150d6838de2c073
NakajimaDaniel <nakajimadaniel96@gmail.com>
2023-03-17 16:30:53 -0300

    feat: new color switch button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,11 +5,28 @@ import { HamburgerIcon } from '@chakra-ui/icons';
 import { Container, Box, Flex, Spacer, Image, Button, useColorMode, Menu, MenuButton, IconButton, MenuList, MenuItem, Icon } from '@chakra-ui/react';
 import { IoMdClose } from 'react-icons/io';
 import { ClientOnly } from '../ClientOnly';
+import {motion} from 'framer-motion'
+import { useState } from 'react';
 
 export function Header() {
 
   const { colorMode, toggleColorMode } = useColorMode();
   const { width } = useDimensions();
+
+  const [ isDarkMode, setIsDarkMode ] = useState();
+
+  function setColorMode() {
+    const newColorMode = !isDarkMode;
+    setIsDarkMode(newColorMode);
+    toggleColorMode();
+  }
+
+  const spring = {
+    type: 'spring',
+    stiffness: 700,
+    damping: 30,
+  }
+  
 
   return (
     <Container maxW="100vw" w="100%" p="0" bg={colorMode === "dark" ? "blue.transparent" : "white.transparent"} position="fixed" top="0" pt="5" pl="5" pb="3" zIndex={20} backdropFilter="saturate(180%) blur(5px)" >
@@ -32,36 +49,26 @@ export function Header() {
             <Box pr={[15, 15, 20, 20, 20]} _hover={{ color: "#FFE072" }} sx={{ 'transition': '0.3s' }} fontSize={[15, 15, 16]} >
               <Link href="/">Home</Link>
             </Box>
-            <Box pr={[15, 15, 20, 20, 20]} _hover={{ color: "#FFE072" }} sx={{ 'transition': '0.3s' }} fontSize={[15, 15, 16]}>
-              <Link href="/projects">Projects</Link>
-            </Box>
+
             <Box pr={[15, 15, 20, 20, 20]} _hover={{ color: "#FFE072" }} sx={{ 'transition': '0.3s' }} fontSize={[15, 15, 16]}>
               <Link href="/blog">Blog</Link>
             </Box>
-            <Box pr={[10, 10, 10, 20, 50]} _hover={{ color: "#FFE072" }} sx={{ 'transition': '0.3s' }} fontSize={[15, 15, 16]}>
-              <Link href="/contact">Contact</Link>
-            </Box>
-            <Box pr="10">
-              <Button onClick={toggleColorMode}>
-                {colorMode === "dark" ? (
-                  <Image src={"/" + "lightmode.svg"} alt="moon logo" />
-                ) : (
-                  <Image src={"/" + "darkmode.svg"} alt="sun logo" />
-                )}
-              </Button>
-            </Box>
+
+            <Flex w="45px" h="20px" bg={isDarkMode ? "#4380F5" : "#ffffff"} borderRadius={20} alignItems={isDarkMode ? "flex-end" : "flex-start"} direction="column"  onClick={setColorMode} justifyContent="center" mr="10" borderColor={isDarkMode ?  "" : "#D9D9D9"} borderWidth="1px" >
+              <motion.div transition={spring} layout >
+                <Box w="14px" h="14px" bg={isDarkMode ? "#ffffff" : "#D9D9D9"}  borderRadius={20} position="relative" right={isDarkMode ? "1" : ""} left={isDarkMode ? "" : "1"}  > </Box>
+              </motion.div>
+            </Flex>
 
           </>
         ) : (
           <>
             <Box pr="7">
-              <Button onClick={toggleColorMode}>
-                {colorMode === "dark" ? (
-                  <Image src={"/" + "lightmode.svg"} alt="moon logo" />
-                ) : (
-                  <Image src={"/" + "darkmode.svg"} alt="sun logo" />
-                )}
-              </Button>
+              <Flex w="45px" h="20px" bg={isDarkMode ? "#4380F5" : "#ffffff"} borderRadius={20} alignItems={isDarkMode ? "flex-end" : "flex-start"} direction="column"  onClick={setColorMode} justifyContent="center" mr="10" borderColor={isDarkMode ?  "" : "#D9D9D9"} borderWidth="1px" >
+                <motion.div transition={spring} layout >
+                  <Box w="14px" h="14px" bg={isDarkMode ? "#ffffff" : "#D9D9D9"}  borderRadius={20} position="relative" right={isDarkMode ? "1" : ""} left={isDarkMode ? "" : "1"}  > </Box>
+                </motion.div>
+              </Flex>
             </Box>
 
             <Box pr="5">
@@ -77,15 +84,10 @@ export function Header() {
                       <Link href="/" passHref>
                         <MenuItem>Home</MenuItem>
                       </Link>
-                      <Link href="/projects" passHref>
-                        <MenuItem>Projects</MenuItem>
-                      </Link>
                       <Link href="/blog" replace passHref>
                         <MenuItem>Blog</MenuItem>
                       </Link>
-                      <Link href="/contact" passHref>
-                        <MenuItem>Contact</MenuItem>
-                      </Link>
+
                     </MenuList>
                   </>
                 )}
