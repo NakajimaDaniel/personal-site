a7fd099a8a2486e59e86f81cf43a4a19028f80f9
NakajimaDaniel <nakajimadaniel96@gmail.com>
2023-03-27 12:17:39 -0300

    fix: fixed code slice language for syntax highlight

--- slices/Code/index.js
+++ slices/Code/index.js
@@ -43,7 +43,7 @@ const Code = ({ slice }) => {
               </SyntaxHighlighter>
               </Box>
             ) : (
-              <SyntaxHighlighter language="javascript">
+              <SyntaxHighlighter language={match[1]}>
               {children}
             </SyntaxHighlighter>
             )
